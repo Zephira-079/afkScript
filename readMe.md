c8496d4849bc00d1b7e32511ab3db203615ea8cc
Zephira-079 <rcph110110@gmail.com>
2024-07-16 01:24:19 +0800

    imtired

--- readMe.md
+++ readMe.md
@@ -1,3 +1,8 @@
 ### version 0.0.0.1
 
 ## initial prototype version
+
+#### type on cmd
+<code>
+    start.bat
+</code>
---
+++ start.bat
@@ -0,0 +1,24 @@
+REM Create a virtual environment in the current directory
+python -m venv env
+
+REM Change to the Scripts directory and activate the virtual environment
+cd env\Scripts
+call activate
+
+REM Move back to the root directory of the project
+cd ../..
+
+REM Install the required packages from requirements.txt
+pip install -r requirements.txt
+
+REM Create a standalone executable with PyInstaller
+pyinstaller --onefile app.py
+
+REM Deactivate the virtual environment
+deactivate
+
+REM Move back to the original directory
+cd ..
+
+echo Setup and build process completed.
+pause
